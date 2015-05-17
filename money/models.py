4c042d778edb51f67e37a938a88b37f5e88405de
Ahmed Sa3d <ahmeds.elghareeb@gmail.com>
2015-05-17 09:50:52 +0200

    init

---
+++ money/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Income(models.Model):
+    pass
+
+
+class Expense(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
--- pocket/urls.py
+++ pocket/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',  TemplateView.as_view(template_name='index.html')),
+    url(r'^dashboard/',  TemplateView.as_view(template_name='dashboard.html')),
 ]
