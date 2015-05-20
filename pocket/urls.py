1d6129afff399f0242b1102633cc6726d214215a
Ahmed Sa3d <ahmeds.elghareeb@gmail.com>
2015-05-21 01:56:51 +0200

    added url to profile.

--- pocket/urls.py
+++ pocket/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',  TemplateView.as_view(template_name='index.html')),
-    url(r'^dashboard/',  TemplateView.as_view(template_name='dashboard.html')),
+    url(r'^accounts/profile/$',  TemplateView.as_view(template_name='dashboard.html')),
+    # url(r'^accounts/', include('accounts.urls')),
     url(r'^accounts/', include('users.urls')),
 ]
