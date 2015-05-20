efe9ddf0c752ca7a6bf9d1025b5dd9d726610eb9
Ahmed Sa3d <ahmeds.elghareeb@gmail.com>
2015-05-21 01:44:07 +0200

    added login page and user in admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
+from .models import User
+from users.admin import UserAdmin
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    list_filter = ('is_active',)
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        (_('Status'), {
+            'fields': ('is_active',)
+        }),
+        (_('Personal Information'), {
+            'fields': ('full_name', 'avatar'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+# from django.conf.urls import url
+# from django.contrib.auth.views import (login, logout)
+
+
+# urlpatterns = [
+#     url(r'^login/$', login, {
+#         'template_name': 'users/login.html',
+#         "redirect_field_name": "dashboard/"}, name='users_login'),
+#     url(r'^logout/$', logout, {'template_name': 'users/logout.html'}, name='users_logout'),
+# ]
