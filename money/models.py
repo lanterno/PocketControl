988bd514366a87412196b5a881d58016132072d1
Ahmed Sa3d <ahmeds.elghareeb@gmail.com>
2015-05-21 08:33:23 +0200

    added Money models and views.

--- money/models.py
+++ money/models.py
@@ -1,13 +1,29 @@
+import datetime
 from django.db import models
+from accounts.models import User
+
+
+class IncomeCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ExpenseCategory(models.Model):
+    name = models.CharField(max_length=100)
 
 
 class Income(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name='income')
+    money = models.FloatField(default=0)
+    category = models.ForeignKey(IncomeCategory)
 
 
 class Expense(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name='expenses')
+    money = models.FloatField(default=0)
+    category = models.ForeignKey(ExpenseCategory)
+    details = models.CharField(max_length=255, null=True, blank=True)
+    date_time = models.DateTimeField()
 
-
-class Category(models.Model):
-    pass
+    def save(self, *args, **kwargs):
+        self.date_time = datetime.datetime.now()
+        super(Expense, self).save(*args, **kwargs)
--- money/views.py
+++ money/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Expense
 
-# Create your views here.
+
+class LatestExpensesListView(ListView):
+    model = Expense
+
+    def get_context_data(self, **kwargs):
+        latest_expenses = self.request.user.expenses.all()[:5]
+        return {"latest_expenses": latest_expenses}
